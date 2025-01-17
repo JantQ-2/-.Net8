46fe4cb62be00b25d2a858aef89ca06a87dbdef8
JantQ-2 <s2300891@edu.bc.fi>
2025-01-17 10:39:24 +0200

    Raylib Testi

--- DVD/DVD/DVD/Program.cs
+++ DVD/DVD/DVD/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace DVD
+﻿using Raylib_cs;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DVD
 {
     internal class Program
     {
-        static void Main(string[] args)
+       
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vector2 A = new Vector2(800 / 2, 0);
+            Vector2 B = new Vector2(0, 800 / 2);
+            Vector2 C = new Vector2(800, 600);
+
+            Raylib.InitWindow(800, 800, "DVD");
+
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                Raylib.DrawLineV(A, B, Color.Green);
+                Raylib.DrawLineV(B, C, Color.Yellow);
+                Raylib.DrawLineV(C, A, Color.SkyBlue);
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
     }
 }
