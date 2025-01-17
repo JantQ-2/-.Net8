33c7ca66159611deac410212afd529d3cb266696
JantQ-2 <s2300891@edu.bc.fi>
2025-01-17 12:46:13 +0200

    DVD

--- DVD/DVD/DVD/Program.cs
+++ DVD/DVD/DVD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DVD
 {
@@ -9,31 +10,48 @@ namespace DVD
        
         static void Main(String[] args)
         {
-            Vector2 A = new Vector2(800 / 2, 0);
-            Vector2 B = new Vector2(0, 800 / 2);
-            Vector2 C = new Vector2(800, 600);
-            Vector2 A2 = new Vector2(1, 1);
-            Vector2 B2 = new Vector2(1, -1);
-            Vector2 C2 = new Vector2(-1, 1);
-
-            float speed = 10;
-
+            Vector2 Position = new Vector2(100, 100);
+            Vector2 suunta = new Vector2(1, -1);
+            
+            float nopeus = 50f;
+            string teksti = "DVD";
+            
             Raylib.InitWindow(800, 800, "DVD");
 
-            while (Raylib.WindowShouldClose() == false)
-            {
-                float deltaTime = Raylib.GetFrameTime();
+            // Hae ikkunan leveys ja korkeus
+            int width = Raylib.GetScreenWidth();
+            int height = Raylib.GetScreenHeight();
 
-                A += A2 * speed * deltaTime;
-                B += B2 * speed * deltaTime;
-                C += C2 * speed * deltaTime;
+            
+            int fontSize = 40;
+            Vector2 textSize = Raylib.MeasureTextEx(Raylib.GetFontDefault(), teksti, fontSize, 1);
+
+            while (!Raylib.WindowShouldClose())
+            {
+
+                float frameTime = Raylib.GetFrameTime();
+
+                Position += suunta * nopeus * frameTime;
+
+                if (Position.X + textSize.X >= width || Position.X <= 0)
+                {
+                    suunta = new Vector2(suunta.X * -1, suunta.Y);
+                    Position.X = Math.Clamp(Position.X, 0, width - textSize.X); 
+                }
+
+                // Törmäystarkistus ylhäällä ja alhaalla
+                if (Position.Y + textSize.Y >= height || Position.Y <= 0)
+                {
+                    suunta = new Vector2(suunta.X, suunta.Y * -1); 
+                    Position.Y = Math.Clamp(Position.Y, 0, height - textSize.Y); 
+                }
 
                 Raylib.BeginDrawing();
+
                 Raylib.ClearBackground(Color.Black);
 
-                Raylib.DrawLineV(A , B, Color.Green);
-                Raylib.DrawLineV(B, C, Color.Yellow);
-                Raylib.DrawLineV(C, A, Color.SkyBlue);
+                Raylib.DrawText(teksti, (int)Position.X, (int)Position.Y, fontSize, Color.Yellow);
+
                 Raylib.EndDrawing();
             }
 
