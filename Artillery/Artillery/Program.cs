276806202a58458471b9a3d658e816cd68303842
JantQ-2 <s2300891@edu.bc.fi>
2025-04-01 14:03:10 +0300

    tanks

---
+++ Artillery/Artillery/Program.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Artillery
+{
+    internal class Program
+    {
+
+        public static int ruudunLeveys = 1000;
+        public static int ruudunKorkeus = 750;
+        public static int muunnin = 30;
+
+        public static Vector2 maaX = new Vector2(0, muunnin);
+        public static Vector2 maaY = new Vector2(0, 0);
+
+        static void Main(string[] args)
+        {
+            Raylib.InitAudioDevice();
+            Sound sound = Raylib.LoadSound("Bullet.mp3");
+
+            Raylib.InitWindow(ruudunLeveys, ruudunKorkeus, "Artillery");
+            Raylib.SetTargetFPS(120);
+            int random1 = Raylib.GetRandomValue(10, 100);
+
+
+            while (!Raylib.WindowShouldClose())
+            {
+
+                if (Raylib.IsKeyPressed(KeyboardKey.Space))
+                {
+                    Raylib.PlaySound(sound);
+                }
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawRectangle((int)maaX.X, ruudunKorkeus-random1, muunnin, random1, Color.Green);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
--- TANKS!/TANKS!/Program.cs
+++ TANKS!/TANKS!/Program.cs
@@ -14,31 +14,12 @@ internal class TANKS
 
         while (!Raylib.WindowShouldClose())
         {
-            Tank tankInstance = new Tank();
-            tankInstance.IsKeyDown();
-            Tank.TarkistaRajat();
-
-            foreach (var ammus in Tank.p1Ammukset) ammus.Update();
-            foreach (var ammus in Tank.p2Ammukset) ammus.Update();
+           
 
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.Black);
 
-            Raylib.DrawRectanglePro(
-            new Rectangle(Tank.p1Sijainti.X, Tank.p1Sijainti.Y, Tank.pelaajanKoko.X, Tank.pelaajanKoko.Y),
-            new Vector2(Tank.pelaajanKoko.X / 2, Tank.pelaajanKoko.Y / 2),
-            Tank.p1Kulma,
-            Color.Green);
-
-            Raylib.DrawRectanglePro(
-                new Rectangle(Tank.p2Sijainti.X, Tank.p2Sijainti.Y, Tank.pelaajanKoko.X, Tank.pelaajanKoko.Y),
-                new Vector2(Tank.pelaajanKoko.X / 2, Tank.pelaajanKoko.Y / 2),
-                Tank.p2Kulma,
-                Color.Red);
-
-            foreach (var ammus in Tank.p1Ammukset) ammus.Draw();
-            foreach (var ammus in Tank.p2Ammukset) ammus.Draw();
-
+           
             Raylib.EndDrawing();
         }
         Raylib.CloseWindow();
