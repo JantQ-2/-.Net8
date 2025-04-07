1d3dec3f263df511b115ed8026098223a61a69fa
JantQ-2 <s2300891@edu.bc.fi>
2025-04-07 12:50:52 +0300

    paaaaljon muutoksia

---
+++ Asteroids/Asteroids/Program.cs
@@ -0,0 +1,43 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Asteroids
+{
+    internal class Program
+    {
+        Texture2D playerShipTexture;
+
+        static void Main(string[] args)
+        {
+            Program game = new Program();
+            game.Init();
+        }
+
+        void Init()
+        {
+            Raylib.InitWindow(800, 800, "Asteroids");
+            Raylib.SetTargetFPS(60);
+
+            playerShipTexture = Raylib.LoadTexture("images/more_images/PNG/playerShip1_red.png");
+
+
+            GameLoop();
+
+            Raylib.CloseWindow();
+        }
+
+        void GameLoop()
+        {
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                Raylib.DrawTexture(playerShipTexture, 10, 10, Color.Red);
+
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
--- TANKS!/TANKS!/Program.cs
+++ TANKS!/TANKS!/Program.cs
@@ -7,6 +7,9 @@ internal class TANKS
     public const int ruudunKorkeus = 800;
     public const int ruudunLeveys = 1000;
 
+    public static Vector2 p1sijainti = new Vector2(ruudunLeveys / 10, ruudunKorkeus / 2);
+    public static Vector2 p2sijainti = new Vector2(ruudunLeveys - (ruudunLeveys / 10), ruudunKorkeus / 2);
+
     static void Main(string[] args)
     {
         Raylib.InitWindow(ruudunLeveys, ruudunKorkeus, "TANKS!");
@@ -14,11 +17,12 @@ internal class TANKS
 
         while (!Raylib.WindowShouldClose())
         {
-           
-
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.Black);
 
+
+            Console.WriteLine(p1sijainti);
+            Console.WriteLine(p2sijainti);
            
             Raylib.EndDrawing();
         }
